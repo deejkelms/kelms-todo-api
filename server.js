f3ef29a53c27166e783a350d5bcc719b8dc18472
Derek Kelmanson <dkelmanson@gmail.com>
2017-01-27 14:53:35 -0500

    Add ?completed filter

--- server.js
+++ server.js
@@ -13,22 +13,26 @@ app.get('/', function(req, res) {
     res.send('Todo API Root');
 });
 
+
+// GET /todos?completed=true
 app.get('/todos', function(req, res){
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    if ( queryParams.hasOwnProperty('completed') && queryParams.completed === 'true' ) {
+      filteredTodos = _.where(filteredTodos, {completed: true});
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+      filteredTodos = _.where(filteredTodos, {completed: false});
+    }
+
+    res.json(filteredTodos);
 });
 
+// GET /todos/:id
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos, {id: todoId});
-    //    OR                            //
-    // var matchedTodo;                 //
-    //                                  //
-    // todos.forEach(function (todo) {  //
-    //   if (todoId === todo.id) {      //
-    //     matchedTodo = todo;          //
-    //   }                              //
-    // });                              //
-
+          
     if (matchedTodo) {
         res.json(matchedTodo);
     } else {
@@ -36,6 +40,7 @@ app.get('/todos/:id', function(req, res){
     }
 });
 
+// POST /todos
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
@@ -54,6 +59,7 @@ app.post('/todos', function (req, res) {
     res.json(body);
 });
 
+// DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos, {id: todoId});
@@ -66,6 +72,7 @@ app.delete('/todos/:id', function (req, res) {
     }
 });
 
+// PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos, {id: todoId});
