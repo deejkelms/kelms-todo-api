2c5959702f505548af5b873454e7630a29923c27
Derek Kelmanson <dkelmanson@gmail.com>
2017-02-07 14:35:39 -0500

    convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -17,22 +17,28 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=true&q=work
 app.get('/todos', function(req, res){
-    var queryParams = req.query;
-    var filteredTodos = todos;
+    var query = req.query;
+    var where = {};
 
-    if ( queryParams.hasOwnProperty('completed') && queryParams.completed === 'true' ) {
-      filteredTodos = _.where(filteredTodos, {completed: true});
-    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-      filteredTodos = _.where(filteredTodos, {completed: false});
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
+      where.completed = true;
+    } else if(query.hasOwnProperty('completed') && query.completed === 'false') {
+      where.completed = false;
     }
 
-    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-      filteredTodos = _.filter(filteredTodos, function(todo) {
-        return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-      });
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+      where.description = {
+        $like: '%' + query.q + '%'
+      };
     }
 
-    res.json(filteredTodos);
+    db.todo.findAll({
+      where: where
+    }).then(function (todos){
+      res.json(todos);
+    }, function (e){
+      res.status(500).send();
+    })
 });
 
 // GET /todos/:id
@@ -64,19 +70,6 @@ app.post('/todos', function (req, res) {
       res.status(400).json(e);
     });
 
-    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    //   return res.status(400).send();
-    // }
-    //
-    // body.description = body.description.trim();
-    //
-    // // add id field to body
-    // body.id = todoNextId++;
-    //
-    // // push body into array
-    // todos.push(body);
-    //
-    // res.json(body);
 });
 
 // DELETE /todos/:id
